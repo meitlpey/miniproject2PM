b63c5f22b93648b62932dd09d1fca8ced5f7d328
meitlpey <prmeitler@mail.fhsu.edu>
2023-09-23 11:40:16 -0500

    In main.py:
    Using Yfinace got 365 days of data for whatever stock is hard coded into the code.
    
    Using matplotlib graphed the 365 days worth of data.
    Added labels like title, ylabel, xlabel and added a basic legend to the graph.
    
    Using np.arange made a trendline for the years worth of data, trendline is based on the average price. Also made the trendline green if the trendline was positive and red if the trendline was negitive.

--- main.py
+++ main.py
@@ -2,18 +2,46 @@
 # Peyton Meitler
 # Mini Project 2
 
-
-#(5/5 points) Initial comments with your name, class and project at the top of your .py file.
-#(5/5 points) Proper import of packages used.
-#(20/20 points) Using a data source of your choice, such as data from data.gov or using the Faker package, generate or retrieve some data for creating basic statistics on. This will generally come in as json data, etc.
-#Think of some question you would like to solve such as:
-#"How many homes in the US have access to 100Mbps Internet or more?"
-#"How many movies that Ridley Scott directed is on Netflix?" - https://www.kaggle.com/datasets/shivamb/netflix-shows
-#Here are some other great datasets: https://www.kaggle.com/datasets
-#(10/10 points) Store this information in Pandas dataframe. These should be 2D data as a dataframe, meaning the data is labeled tabular data.
-#(10/10 points) Using matplotlib, graph this data in a way that will visually represent the data. Really try to build some fancy charts here as it will greatly help you in future homework assignments and in the final project.
 #(10/10 points) Save these graphs in a folder called charts as PNG files. Do not upload these to your project folder, the project should save these when it executes. You may want to add this folder to your .gitignore file.
 #(10/10 points) There should be a minimum of 5 commits on your project, be sure to commit often!
 #(10/10 points) I will be checking out the master branch of your project. Please be sure to include a requirements.txt file which contains all the packages that need installed. You can create this fille with the output of pip freeze at the terminal prompt.
 #(20/20 points) There should be a README.md file in your project that explains what your project is, how to install the pip requirements, and how to execute the program. Please use the GitHub flavor of Markdown. Be thorough on the explanations.
+
+import yfinance as yf
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import stats
+
+# gets information on whatever stock you enter
+stock = yf.Ticker("AAPL")
+hist = stock.history(period="365d") # get historical market data from the past year
+
+# Extract the date and closing price
+dates = hist.index
+closing_prices = hist['Close']
+
+# Calculate the trend line
+x = np.arange(len(dates))
+slope, intercept, r_value, p_value, std_err = stats.linregress(x, closing_prices)
+trend_line = slope * x + intercept
+
+# Determine the color of the trend line based on its slope
+trend_line_color = 'green' if slope > 0 else 'red'
+
+# Plot the closing price and trend line
+plt.figure(figsize=(10, 6))
+plt.title("Closing Price for Microsoft (MSFT) with Trend Line")
+plt.ylabel("Closing Price")
+plt.xlabel("Date")
+
+plt.plot(dates, closing_prices, label="Closing Price", color='blue')
+plt.plot(dates, trend_line, label="Trend Line", color=trend_line_color, linestyle='-')
+
+plt.legend()
+plt.grid(True)
+
+plt.show()
+
+
+
 
